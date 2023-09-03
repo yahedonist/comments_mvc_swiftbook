2baea7b2b62d94d1f7bfaf755d1ec0c5a3a3de70
yahedonist <yatopolskov@gmail.com>
2023-09-03 14:50:24 +0300

    Initial Commit

---
+++ comments_mvc_swiftbook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  comments_mvc_swiftbook
+//
+//  Created by Nikita on 03.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
